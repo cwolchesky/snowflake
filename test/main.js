d96f43a8ea1f9f5898510561810a0aec8eb78a0a
Chris Wolchesky <chris@wolchesky.net>
2017-07-18 14:10:42 -0400

    Oops, didn't commit the .travis.yml and tests directory

---
+++ test/main.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+// Sanity Check
+describe('Mocha', function() {
+    it('should pass a sanity check', function() {
+        expect(true).to.be.ok;
+    });
+});
