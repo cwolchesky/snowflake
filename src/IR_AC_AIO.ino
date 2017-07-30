de742eb0f5e9c57940a4f042639b8e65fb3267ad
Chris Wolchesky <chris@wolchesky.net>
2017-07-29 23:14:48 -0400

    +Finished Arduino sketch.  Tested circuit with it. +No issues in Arduino sketch, operates as expected. TODO: Document the code.

--- src/IR_AC_AIO.ino
+++ src/IR_AC_AIO.ino
@@ -4,19 +4,27 @@
 // and any additional configuration needed for WiFi, cellular,
 // or ethernet clients.
 #include "config.h"
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include "IRCommands.h"
+
 
 // Include DHT Sensor Library
 #include "DHT.h"
 
+
+
 #define DHTPIN 12
+#define IRPin 15
 #define DHTTYPE DHT11
+#define LEDPin 13
 
 DHT dht(DHTPIN,DHTTYPE);
 
-/************************ Example Starts Here *******************************/
+IRsend irsend(15);
+IRCommands ircom;
 
-// this int will hold the current count for our sketch
-int mode = 0;
+int updateMode = 0;
 
 // set up the 'counter' feed
 AdafruitIO_Feed *snowflake = io.feed("snowflake");
@@ -27,15 +35,20 @@ AdafruitIO_Feed *snowflake_heatIndex = io.feed("snowflake_heatIndex");
 
 void setup() {
 
+  int timeOutCount = 0;
+  int timeOut = 60;
+  
   // start the serial connection
   Serial.begin(115200);
 
   // wait for serial monitor to open
-  while(! Serial);
+  //while(! Serial);
 
-  Serial.print("\n\nConnecting to Adafruit IO");
+  Serial.println("\n\nConnecting to Adafruit IO");
   dht.begin();
   Serial.println("Beginning to monitor temperature");
+  irsend.begin();
+  Serial.println("Beginning IRSend");
 
   // connect to io.adafruit.com
   io.connect();
@@ -48,13 +61,21 @@ void setup() {
 
   // wait for a connection
   while(io.status() < AIO_CONNECTED) {
+    timeOutCount++;
     Serial.print(".");
     delay(500);
+    if(timeOutCount >= timeOut) {
+      Serial.println();
+      Serial.print("Time out on connecting to AIO");
+      return;
+    }
   }
 
   // we are connected
   Serial.println();
   Serial.println(io.statusText());
+
+  pinMode(LEDPin, OUTPUT);
 
 }
 
@@ -72,26 +93,26 @@ void loop() {
   float hif = dht.computeHeatIndex(f, h);
   
   // Alternate sensor reading to update to stay below throttle limits:
-  switch (mode) {
+  switch (updateMode) {
     case 0:
       Serial.println("Sending Temp (F) update");
       snowflake_temp->save(f);
-      mode = 1;
+      updateMode = 1;
       break;
     case 1:
       Serial.println("Sending Temp (C) update");
       snowflake_tempC->save(c);
-      mode = 2;
+      updateMode = 2;
       break;
     case 2:
       Serial.println("Sending Humidity update");
       snowflake_humidity->save(h);
-      mode = 3;
+      updateMode = 3;
       break;
     case 3:
       Serial.println("Sending Heat Index update");
       snowflake_heatIndex->save(hif);
-      mode = 0;
+      updateMode = 0;
       break;
     default:
       Serial.println("Default case caught, no update made!");
@@ -100,7 +121,7 @@ void loop() {
   
 
   // wait one second (1000 milliseconds == 1 second)
-  delay(2000);
+  delay(2500);
 
 }
 
@@ -109,7 +130,90 @@ void loop() {
 // the counter feed in the setup() function above.
 void handleMessage(AdafruitIO_Data *data) {
 
-  Serial.print("received <- ");
+  Serial.print("DEBUG: Received data->value ");
   Serial.println(data->value());
+  int command = data->toInt();
+  /*
+   * 0 = Power
+   * 2 = Fan up
+   * 3 = Fan down
+   * 4 = Temp up
+   * 5 = Temp down
+   * 6 = Cool Mode
+   * 7 = Fan Only
+   * 8 = Energy Saver
+   * 9 = Auto Fan
+   * 10 = Sleep
+   * 11 = Timer
+   */
+
+  switch (command) {
+    case 0:
+      Serial.println("Got Power Button");
+      irsend.sendRaw(ircom.power, 68, 38);
+      flashLED();
+      break;
+    case 2:
+      Serial.println("Got fan up");
+      irsend.sendRaw(ircom.fanUp, 68, 38);
+      flashLED();
+      break;
+    case 3:
+      Serial.println("Got fan down");
+      irsend.sendRaw(ircom.fanDown, 68, 38);
+      flashLED();
+      break;
+    case 4:
+      Serial.println("Got temp up");
+      irsend.sendRaw(ircom.tempUp, 68, 38);
+      flashLED();
+      break;
+    case 5:
+      Serial.println("Got temp down");
+      irsend.sendRaw(ircom.tempDown, 68, 38);
+      flashLED();
+      break;
+    case 6:
+      Serial.println("Got cool mode");
+      irsend.sendRaw(ircom.coolMode, 68, 38);
+      flashLED();
+      break;
+    case 7:
+      Serial.println("Got fan only mode");
+      irsend.sendRaw(ircom.fanOnlyMode, 68, 38);
+      flashLED();
+      break;
+    case 8:
+      Serial.println("Got energy saver mode");
+      irsend.sendRaw(ircom.energySaverMode, 68, 38);
+      flashLED();
+      break;
+    case 9:
+      Serial.println("Got Auto Fan Mode");
+      irsend.sendRaw(ircom.autoFanMode, 68, 38);
+      flashLED();
+      break;
+    case 10:
+      Serial.println("Got sleep button press");
+      irsend.sendRaw(ircom.sleepButton, 68, 38);
+      flashLED();
+      break;
+    case 11:
+      Serial.println("Got timer button press");
+      irsend.sendRaw(ircom.timerButton, 68, 38);
+      flashLED();
+      break;
+    default:
+      Serial.print("Got unrecognized command: ");
+      Serial.println(command);
+    break;
+  }
 
 }
+
+void flashLED() {
+  digitalWrite(LEDPin, HIGH);
+  delay(100);
+  digitalWrite(LEDPin, LOW);
+}
+
